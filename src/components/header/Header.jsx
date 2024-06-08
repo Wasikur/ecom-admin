c5911a3a77afc3cb1b101f881748d64db8040001
Wasikur <rahmankhanwasikur@gmail.com>
2024-06-08 17:57:30 +0530

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -46,7 +46,6 @@ const Header = () => {
   const displayName = currentUser?.displayName || currentUser?.email;
 
   return (
-<<<<<<< HEAD
     <div className="flex justify-between h-20 w-full p-6 bg-stone-50 select-none border-b-2 shadow-xl">
       {isMobile ? (
         <span className="text-xl font-semibold">
@@ -55,22 +54,10 @@ const Header = () => {
       ) : (
         <TypeAnimation
           sequence={[`Welcome,`, 1000, `${displayName || "Guest"}`, 1000]}
-=======
-    <>
-      <div className="flex justify-between h-20 w-full p-6 bg-stone-50 select-none border-b-2 shadow-xl">
-{/*         <TypeAnimation
-          sequence={[
-            `Welcome`,
-            1000, // wait 1s before replacing "Mice" with
-            `${displayName}`,
-            1000,
-          ]}
->>>>>>> 7ef13a4c326688f24e76e585be1261cab56b1718
           wrapper="span"
           speed={50}
           style={{ fontSize: "1.5rem", fontWeight: 600 }}
           repeat={Infinity}
-<<<<<<< HEAD
         />
       )}
       <div className="flex flex-row items-center gap-10">
@@ -80,19 +67,6 @@ const Header = () => {
           </Button>
         </Link>
         <AvatarButton />
-=======
-        /> */}
-        <div className="flex flex-row items-center gap-10">
-          {/* <Bell /> */}
-          <Link to="/">
-            <Button className="bg-indigo-500 font-bold hover:bg-indigo-600 transition-colors">
-              {" "}
-              Visit Website
-            </Button>
-          </Link>
-          <AvatarButton />
-        </div>
->>>>>>> 7ef13a4c326688f24e76e585be1261cab56b1718
       </div>
     </div>
   );
